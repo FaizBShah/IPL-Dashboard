c1a996fb42fd32343acb750c5859c5679fab5b60
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2021-05-04 16:43:59 +0530

    Created routes for Team pages

--- src/frontend/src/components/MatchDetailCard.js
+++ src/frontend/src/components/MatchDetailCard.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export const MatchDetailCard = ({match}) => {
-
+export const MatchDetailCard = ({teamName, match}) => {
   if (!match) return null;
+  const otherTeam = teamName === match.team1 ? match.team2 : match.team1;
+
   return (
     <div className="MatchDetailCard">
       <h3>Latest Matches</h3>
-      <h4>Match Details</h4>
-      <h4>{match.team1} vs {match.team2}</h4>
+      <h1>vs <Link to={`/teams/${otherTeam}`}>{otherTeam}</Link></h1>
+      <h2>{match.date}</h2>
+      <h3>{match.venue}</h3>
+      <h3>{match.matchWinner} won by {match.resultMargin} {match.result}</h3>
     </div>
   )
 }
