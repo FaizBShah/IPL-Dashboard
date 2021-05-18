6e13c141fcf78f0232422d59a3214e16ea381ad6
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2021-05-18 19:21:31 +0530

    Team Page UI made

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 import './TeamPage.scss';
+import { PieChart } from 'react-minimal-pie-chart';
 
 export const TeamPage = () => {
   const [team, setTeam] = useState({matches: []});
@@ -28,6 +29,12 @@ export const TeamPage = () => {
       </div>
       <div className="win-loss-section">
         Wins / Losses
+        <PieChart
+          data={[
+            {title: 'Losses', value: team.totalMatches - team.totalWins, color: "#a34d5d"},
+            {title: 'Wins', value: team.totalWins, color: "#4da375"},
+          ]}
+        />
       </div>
       <div className="match-detail-section">
         <h3>Latest Matches</h3>
